bb2f072d2bb9b562183f75e30a3f68c393604e52
KenCuevas <20182146@unapec.edu.do>
2021-07-20 18:29:10 -0400

    Mejoras visuales en gestion ingresos (create y edit)

--- FinanzApp/Models/GestionIngreso.cs
+++ FinanzApp/Models/GestionIngreso.cs
@@ -13,13 +13,13 @@ namespace FinanzApp.Models
         [Display(Name = "Tipo de ingreso")]
         public int IdTipoIngreso { get; set; }
         [Required(ErrorMessage = "Debes ingresar la descripcion")]
-        [Range(0, 250, ErrorMessage = "Debes ingresar valores entre 0 y 250 caracteres")]
+        [Range(0, 50, ErrorMessage = "Debes ingresar valores entre 0 y 250 caracteres")]
         public string Descripcion { get; set; }
         [Required(ErrorMessage ="Ingresa la fuente del ingreso")]
         public string Fuente { get; set; }
         [Required(ErrorMessage = "Debes seleccionar el estado (ACTIVO O INACTIVO)")]
         public bool Estado { get; set; }
-
+        [Display(Name = "Tipo de ingreso")]
         public virtual TiposIngreso IdTipoIngresoNavigation { get; set; }
     }
 }
